734099f2b1af12145a4707496c38ddbdf80fca78
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-03 22:04:07 +0200

    Use app.route to /articles endpoint

--- api.js
+++ api.js
@@ -28,17 +28,6 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema);
 
-// Get endpoints
-app.get("/articles", async (req, res) => {
-  try {
-    const articles = await Article.find({});
-    res.send(articles);
-  } catch (err) {
-    res.send(err.message);
-    console.err(err);
-  }
-});
-
 app.get("/articles/:title", async (req, res) => {
   try {
     const title = req.params.title;
@@ -55,32 +44,43 @@ app.get("/", (req, res) => {
   res.send("Welcome.");
 });
 
-// Post endpoints
-// Add a new article
-app.post("/articles", async (req, res) => {
-  const article = new Article({
-    title: req.body.title,
-    content: req.body.content,
+// Route to articles endpoint
+app
+  .route("/articles")
+  // All articles
+  .get(async (req, res) => {
+    try {
+      const articles = await Article.find({});
+      res.send(articles);
+    } catch (err) {
+      res.send(err.message);
+      console.err(err);
+    }
+  })
+  // Post one article
+  .post(async (req, res) => {
+    const article = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    try {
+      await article.save();
+      res.send(`Article with title: ${title} succesfully added.`);
+    } catch (err) {
+      console.error(err);
+      res.send(err.message);
+    }
+  })
+  // Delete all articles
+  .delete(async (req, res) => {
+    try {
+      await Article.deleteMany({});
+      res.send("All articles succesfully deleted.");
+    } catch (err) {
+      console.error(err);
+      res.send(err.message);
+    }
   });
-  try {
-    await article.save();
-    res.send(`Article with title: ${title} succesfully added.`);
-  } catch (err) {
-    console.error(err);
-    res.send(err.message);
-  }
-});
-
-// Delete all articles
-app.delete("/articles", async (req, res) => {
-  try {
-    await Article.deleteMany({});
-    res.send("All articles succesfully deleted.");
-  } catch (err) {
-    console.error(err);
-    res.send(err.message);
-  }
-});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000.");
